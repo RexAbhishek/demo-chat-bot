f04dbf555562e7654651ae83314eda339796f9e9
“RexAbhishek” <abhishekrajamar@gmail.com>
2024-10-17 22:16:48 +0530

    minor changes4

--- config/open-ai.js
+++ config/open-ai.js
@@ -2,9 +2,7 @@ import { OpenAI } from "openai";
 import dotenv from "dotenv";
 dotenv.config();
 
-// const configuration = new Configuration({
-//     apiKey: process.env.API_KEY
-// });
+
 
 const openai = new OpenAI({
     apiKey: 
